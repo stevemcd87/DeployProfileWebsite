5a175b9a15b49457e9c31f2fb3cce101f18ab6e6
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-08-27 15:18:56 -0400

    added routes for Roter in HElloWorld.jsx

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,13 +1,20 @@
 class ProfileController < ApplicationController
-  def create
-    @profile = Profile.new(profile_params)
+  def show
+    @profile = Profile.first
+    render json: @profile
+  end
 
+  def create
+    redirect_to hello_world_path if Profile.all.count >= 1
+    @profile = Profile.new(profile_params)
     @profile.save
-    redirect_to @profile
+    redirect_to hello_world_path
   end
 
   private
-    def profile_params
-      params.require(:profile).permit(:first_name, :middle_name, :last_name, :email, :phone_number)
-    end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :middle_name, :last_name, :email, :phone_number)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:create]
+  resources :articles, only: [:create, :show]
   get 'hello_world', to: 'hello_world#index'
+  get 'about-me', to: 'hello_world#index'
+  get 'about-me/*story', to: 'hello_world#index'
+  get 'blogs', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

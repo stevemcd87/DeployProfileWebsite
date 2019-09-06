6bc9b5e3a9423a5b8d7201d3e436478fd8c2f907
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-09-06 08:16:41 -0400

    refactored tech_field method in ProjectsHelper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,8 @@
 module ProjectsHelper
 
   def tech_field(project)
-    edit_project_tech = text_field(:project, :technologies, value: @project[:technologies].join(','))
-    add_project_path = text_field(:project, :technologies)
-    project.technologies ? edit_project_tech : add_project_path
+    technologies_value = project.technologies ? @project[:technologies].join(',') : ""
+    text_field(:project, :technologies, value: technologies_value)
   end
-  
+
 end

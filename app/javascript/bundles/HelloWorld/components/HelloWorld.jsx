c7125f9aabaec2db004cff5df88230db1bd132f7
Anibal <centrogabinetesanibal@gmail.com>
2019-09-07 13:06:10 -0400

    - Added js_include_tag to hello_world - Added links for devise in hello world index.html.erb - Edit and Reset buttons only work when signed in

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,17 +1,17 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
-import AboutMe from './about-me/AboutMe';
-import SocialNetworks from './SocialNetworks';
-import Projects from './Projects';
-import Blog from './Blog';
-import ProfileHeader from './profile/ProfileHeader';
+import AboutMe from "./about-me/AboutMe";
+import SocialNetworks from "./SocialNetworks";
+import Projects from "./Projects";
+import Blog from "./Blog";
+import ProfileHeader from "./profile/ProfileHeader";
 
 export default class HelloWorld extends React.Component {
   static propTypes = {
-    profile: PropTypes.object, // .isRequired - this is passed from the Rails view
+    profile: PropTypes.object,
+    signedIn: PropTypes.bool.isRequired // .isRequired - this is passed from the Rails view
   };
   /**
    * @param props - Comes from your rails view.
@@ -20,48 +20,83 @@ export default class HelloWorld extends React.Component {
     super(props);
     // How to set initial state in ES6 class syntax
     // https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
-    this.state = { profile: this.props.profile };
+
+    this.state = {
+      profile: this.props.profile,
+      signedIn: this.props.signedIn
+    };
   }
 
-  setProfileState = (profile) => {
-    this.setState({profile: profile})
-  }
+  setProfileState = profile => {
+    this.setState({
+      profile: profile
+    });
+  };
 
   render() {
     let profile = this.state.profile,
-      socialNetworks = profile && profile.social_networks.length > 0 ? profile.social_networks : null;
+      signedIn = this.state.signedIn,
+      socialNetworks =
+        profile && profile.social_networks.length > 0
+          ? profile.social_networks
+          : null;
     console.log(this.state);
     return (
       <div id="hello-world">
-        {!profile && <a href="/profiles/new">Create Profile</a>}
-        {profile &&
+        {" "}
+        {!profile && <a href="/profiles/new"> Create Profile </a>}{" "}
+        {profile && (
           <div>
-            <ProfileHeader profile={profile} setProfileState={this.setProfileState} />
-            {!socialNetworks && <a href={`/profiles/${profile.id}/social_networks/new`}>Add SocialNetwork</a>}
-            {socialNetworks && <SocialNetworks profile={profile} />}
+            <ProfileHeader
+              signedIn={signedIn}
+              profile={profile}
+              setProfileState={this.setProfileState}
+            />{" "}
+            {!socialNetworks && (
+              <a href={`/profiles/${profile.id}/social_networks/new`}>
+                {" "}
+                Add SocialNetwork{" "}
+              </a>
+            )}{" "}
+            {socialNetworks && <SocialNetworks profile={profile} />}{" "}
             <Router>
               <div>
-
                 <nav id="hello-world-nav" role="navigation">
                   <ul>
                     <li>
-                      <Link to="/about-me">About Me</Link>
-                    </li>
+                      <Link to="/about-me"> About Me </Link>{" "}
+                    </li>{" "}
                     <li>
-                      <Link to="/portfolio">Portfolio</Link>
-                    </li>
+                      <Link to="/portfolio"> Portfolio </Link>{" "}
+                    </li>{" "}
                     <li>
-                      <Link to="/blogs">Blogs</Link>
-                    </li>
-                  </ul>
+                      <Link to="/blogs"> Blogs </Link>{" "}
+                    </li>{" "}
+                  </ul>{" "}
                 </nav>
-
-                <Route path="/about-me/" component={ () => <AboutMe profile={profile} setProfileState={this.setProfileState} /> } />
-                <Route path="/blogs/" component={Blog} />
-                <Route path="/portfolio/" component={ () => <Projects profile={profile} setProfileState={this.setProfileState} /> } />
-              </div>
-            </Router>
-          </div>}
+                <Route
+                  path="/about-me/"
+                  component={() => (
+                    <AboutMe
+                      profile={profile}
+                      setProfileState={this.setProfileState}
+                    />
+                  )}
+                />
+                <Route path="/blogs/" component={Blog} />{" "}
+                <Route
+                  path="/portfolio/"
+                  component={() => (
+                    <Projects
+                      profile={profile}
+                      setProfileState={this.setProfileState}
+                    />
+                  )}
+                />
+              </div>{" "}
+            </Router>{" "}
+          </div>
+        )}{" "}
       </div>
     );
   }
--- app/javascript/bundles/HelloWorld/components/profile/ProfileHeader.jsx
+++ app/javascript/bundles/HelloWorld/components/profile/ProfileHeader.jsx
@@ -1,40 +1,50 @@
-import React from 'react';
+import React from "react";
 
-const ProfileHeader = (props) => {
+const ProfileHeader = props => {
   let profile = props.profile,
+    signedIn = props.signedIn,
     setProfileState = props.setProfileState,
     deleteProfile = () => {
-    let confirmed = confirm("Are you sure?");
-    if (confirmed) {
-      fetch(`/profiles/${profile.id}`, {
-        method: "delete"
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw response;
-          }
-          return response.json();
+      let confirmed = confirm("Are you sure?");
+      if (confirmed) {
+        fetch(`/profiles/${profile.id}`, {
+          method: "delete"
         })
-        .then(res => {
-          setProfileState(res.profile);
-        })
-        .catch(error => {
-          console.error("error", error);
-        });
-    }
-    return false
-  };
+          .then(response => {
+            if (!response.ok) {
+              throw response;
+            }
+            return response.json();
+          })
+          .then(res => {
+            setProfileState(res.profile);
+          })
+          .catch(error => {
+            console.error("error", error);
+          });
+      }
+      return false;
+    };
   return (
     <header id="profile-header" role="banner">
-      <div id="profile-header-buttons" className="hidden">
-        <a href={`/profiles/${profile.id}/edit`} className="profile-header-button">Edit Profile</a>
-        <a href="#" className="profile-header-button" onClick={deleteProfile}>Reset Profile</a>
-      </div>
+      {signedIn && (
+        <div id="profile-header-buttons" className="hidden">
+          <a
+            href={`/profiles/${profile.id}/edit`}
+            className="profile-header-button"
+          >
+            Edit Profile
+          </a>
+          <a href="#" className="profile-header-button" onClick={deleteProfile}>
+            Reset Profile
+          </a>
+        </div>
+      )}
       <h1>{profile.full_name}</h1>
       <h3>{profile.email}</h3>
       <h3>{profile.phone_number}</h3>
     </header>
   );
-}
+};
 
 export default ProfileHeader;
